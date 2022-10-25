152ffc396c0d380698717ff3fc577352d0736fed
VVolkov833 <finnish.ru@gmail.com>
2022-10-25 21:59:24 +0300

    semi-ready

---
+++ first-screen.php
@@ -0,0 +1,353 @@
+<?php
+
+/*
+Plugin Name: First Screen CSS
+Description: Insert <style>...</style> somewhere high and minify it.
+Version: 0.0.1
+Requires at least: 4.7
+Requires PHP: 7.0.0
+Author: Firmcatalyst, Vadim Volkov
+Author URI: https://firmcatalyst.com
+License: GPL v2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: fcpfsc
+*/
+
+namespace FCP\FirstScreenCSS;
+
+defined( 'ABSPATH' ) || exit;
+
+define( 'FCPFSC', [
+    'dev'            => true,
+    'prefix'         => 'fcpfsc' . '-',
+]);
+
+if( !function_exists( 'get_plugin_data' ) ) { require_once( ABSPATH . 'wp-admin/includes/plugin.php' ); }
+// ++check how and where else it is included get_plugin_data
+define( 'FCPFSC_VER', get_plugin_data( __FILE__ )[ 'Version' ] . ( FCPFSC['dev'] ? time() : '' ) );
+
+
+// print the styles
+add_action( 'wp_head', function() { // include the first-screen styles, instead of enqueuing
+
+    $minify = function ($text) {
+        $text = preg_replace( '/\/\*(?:.*?)*\*\//', '', $text ); // remove comments
+        $text = preg_replace( '/\s+/', ' ', $text ); // one-line & only single speces
+        $text = preg_replace( '/ ?([\{\};:\>\~\+]) ?/', '$1', $text ); // remove spaces
+        $text = preg_replace( '/\+(\d)/', ' + $1', $text ); // restore spaces in functions
+        $text = preg_replace( '/(?:[^\}]*)\{\}/', '', $text ); // remove empty properties
+        $text = str_replace( [';}', '( ', ' )'], ['}', '(', ')'], $text ); // remove last ; and spaces
+        // ++can also remove 0 from 0.5
+        return trim( $text );
+    };
+
+    // collect csss to print on the post
+    $csss = [];
+
+    // get post type
+    $qo = get_queried_object();
+    $post_type = '';
+    if ( is_object( $qo ) ) {
+        if ( get_class( $qo ) === 'WP_Post_Type' ) {
+            $post_type = $qo->name;
+        }
+        if ( get_class( $qo ) === 'WP_Post' ) {
+            $post_type = $qo->post_type;
+        }
+    }
+
+    if ( is_singular( $post_type ) ) {
+        // get css by post id 
+        $css_id = get_post_meta( $qo->ID, FCPFSC['prefix'].'id' )[0];
+        if ( isset( $css_id ) ) {
+            $csss[] = $css_id;
+        }
+        unset( $css_id );
+        // get css by post-type
+        $csss = array_merge( $csss, get_meta_values( FCPFSC['prefix'].'post-types', $post_type ) );
+    }
+    if ( is_home() || is_archive() && ( !$post_type || $post_type === 'page' ) ) {
+        // get css for blog
+        $csss = array_merge( $csss, get_meta_values( FCPFSC['prefix'].'post-archives', $post_type ) );
+    }
+    if ( is_post_type_archive( $post_type ) ) {
+        // get css for custom post type archive
+        $csss = array_merge( $csss, get_meta_values( FCPFSC['prefix'].'post-archives', $post_type ) );
+    }
+
+    ob_start();
+
+    ?>
+    <pre><?php print_r( $csss ) ?></pre>
+    <style id='first-screen-inline-css' type='text/css'><?php
+
+    echo "{works}";
+    
+    ?></style>
+    <?php
+
+    $content = ob_get_contents();
+    ob_end_clean();
+
+    if ( FCPFSC['dev'] ) {  echo $content; return; }
+    echo $minify( $content );
+   
+}, 7 );
+
+// admin post type for css-s
+//++ rebuild manually
+require_once( __DIR__ . '/classes/add-post-type.class.php' );
+use \FCP\FirstScreenCSS\FCPAddPostType as addPostType;
+
+new addPostType([
+    'name' => 'FirstScreen CSS',
+    'type' => 'fcpfsc',
+    'slug' => 'fcpfsc',
+    'plural' => 'FirstScreen CSS',
+    'description' => 'CSS to print before everything',
+    'fields' => ['title'],
+    'hierarchical' => false,
+    'public' => false,
+    'gutenberg' => false,
+    'menu_position' => 23,
+    'menu_icon' => 'dashicons-money-alt',
+    'has_archive' => false
+]);
+
+// admin meta boxes
+add_action( 'add_meta_boxes', function() {
+    add_meta_box(
+        'first-screen-css',
+        'First screen CSS',
+        'FCP\FirstScreenCSS\fcpfsc_meta_box',
+        ['fcpfsc'], // ++modify like the billing - so no publish button
+        'normal',
+        'high'
+    );
+    add_meta_box(
+        'first-screen-css',
+        'First screen CSS',
+        'FCP\FirstScreenCSS\anypost_meta_box',
+        ['page'], //++ all public
+        'side',
+        'low'
+    );
+});
+
+function fcpfsc_meta_box() {
+    global $post;
+
+    // get post types to print options
+    $post_types = get_post_types( [], 'objects' );
+    $public_post_types = [];
+    $archives_post_types = [];
+    foreach ( $post_types as $type ) {
+        $type->name = isset( $type->rewrite->slug ) ? $type->rewrite->slug : $type->name;
+        if ( $type->public ) {
+            $public_post_types[ $type->name ] = $type->label;
+        }
+        if ( $type->has_archive ) {
+            $archives_post_types[ $type->name ] = $type->label;
+        }
+    }
+
+
+    textarea( (object) [
+        'name' => 'css',
+        'placeholder' => 'First Screen inline CSS',
+        'value' => get_post_meta( $post->ID, FCPFSC['prefix'].'css' )[0],
+    ]);
+
+    ?><p><strong>Apply to the following post types</strong></p><?php
+
+    checkboxes( (object) [
+        'name' => 'post-types',
+        'options' => $public_post_types,
+        'value' => get_post_meta( $post->ID, FCPFSC['prefix'].'post-types' )[0],
+    ]);
+
+    ?><p><strong>Apply to the Archive pages of the following post types</strong></p><?php
+
+    checkboxes( (object) [
+        'name' => 'post-archives',
+        'options' => $archives_post_types,
+        'value' => get_post_meta( $post->ID, FCPFSC['prefix'].'post-archives' )[0],
+    ]);
+
+    ?>
+    <p>Pick the first screen css in a separate meta box, added to every post-type on the website</p>
+    <p>Grab the first screen css with a script: <a href="https://github.com/VVolkov833/first-screen-css-grabber" target="_blank" rel="noopener">github.com/VVolkov833/first-screen-css-grabber</a></p>
+    <?php
+
+    wp_nonce_field( FCPFSC['prefix'].'nounce-action', FCPFSC['prefix'].'nounce-name' );
+}
+
+function anypost_meta_box() {
+    global $post;
+
+    // get css post types
+    $css_posts0 = get_posts([
+        'post_type' => 'fcpfsc',
+        'orderby' => 'post_title',
+        'order'   => 'ASC',
+        'post_status' => ['any', 'active'],
+        'posts_per_page' => -1,
+    ]);
+    $css_posts = [];
+    foreach( $css_posts0 as $v ){
+        $css_posts[ $v->ID ] = $v->post_title ? $v->post_title : __( '(no title)' );
+    }
+
+    select( (object) [
+        'name' => 'id',
+        'placeholder' => 'Select the FirstScreen css',
+        'options' => $css_posts,
+        'value' => get_post_meta( $post->ID, FCPFSC['prefix'].'id' )[0],
+    ]);
+
+    wp_nonce_field( FCPFSC['prefix'].'nounce-action', FCPFSC['prefix'].'nounce-name' );
+}
+
+// style meta boxes
+add_action( 'admin_footer', function() {
+    //++post type or screen filter
+    ?><style type="text/css">
+#first-screen-css textarea {
+    width:100%;
+    height:60vh;
+}
+#first-screen-css select {
+    width:100%;
+    box-sizing:border-box;
+}
+#first-screen-css p {
+    margin:30px 0 10px;
+}
+#first-screen-css p + p {
+    margin-top:10px;
+}
+    </style><?php
+});
+
+// save meta data
+add_action( 'save_post', function( $postID ) {
+    if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) { return; }
+    if ( !wp_verify_nonce( $_POST[ FCPFSC['prefix'].'nounce-name' ], FCPFSC['prefix'].'nounce-action' ) ) { return; }
+    if ( !current_user_can( 'edit_post', $postID ) ) { return; }
+
+    $post = get_post( $postID );
+    if ( $post->post_type === 'revision' ) { return; }
+
+    if ( $post->post_type === 'fcpfsc' ) {
+        $fields = [ 'css', 'post-types', 'post-archives' ];
+    } else {
+        $fields = [ 'id' ];
+    }
+
+    foreach ( $fields as $f ) {
+        $f = FCPFSC['prefix'] . $f;
+        if ( empty( $_POST[ $f ] ) ) {
+            delete_post_meta( $postID, $f );
+            continue;
+        }
+        update_post_meta( $postID, $f, $_POST[ $f ] );
+    }
+});
+//require_once( __DIR__ . '/classes/add-post-type.class.php' );
+
+
+//use FCPAddPostType as AddType;
+
+// add post type for the styles
+
+// add needed meta fields
+
+// apply meta boxes to the pages
+
+// apply current style to a post type (like all posts)
+
+// maybe add arcnives?
+
+function textarea($a) {
+    ?>
+<textarea
+    name="<?php echo FCPFSC['prefix'] . $a->name ?>"
+    id="<?php echo FCPFSC['prefix'] . $a->name ?>"
+    rows="<?php echo isset( $a->rows ) ? $a->rows : '10' ?>" cols="<?php echo isset( $a->cols ) ? $a->cols : '50' ?>"
+    placeholder="<?php echo isset( $a->placeholder ) ? $a->placeholder : '' ?>"
+    class="<?php echo isset( $a->className ) ? $a->className : '' ?>"
+><?php
+    echo esc_textarea( isset( $a->value ) ? $a->value : '' )
+?></textarea>
+    <?php
+}
+
+function checkboxes($a) {
+    ?>
+<fieldset
+    id="<?php echo FCPFSC['prefix'] . $a->name ?>"
+    class="<?php echo isset( $a->className ) ? $a->className : '' ?>"><?php
+
+    foreach ( (array) $a->options as $k => $v ) {
+        $checked = is_array( $a->value ) && in_array( $k, $a->value );
+    ?><label>
+        <input type="checkbox"
+            name="<?php echo FCPFSC['prefix'] . $a->name ?>[]"
+            value="<?php echo esc_attr( $k ) ?>"
+            <?php echo $checked ? 'checked' : '' ?>
+        >
+        <span><?php echo $v ?></span>
+    </label><?php } ?>
+</fieldset>
+    <?php
+}
+
+function select($a) {
+    ?>
+    <select
+        name="<?php echo FCPFSC['prefix'] . $a->name ?>"
+        id="<?php echo FCPFSC['prefix'] . $a->name ?>"
+        class="<?php echo isset( $a->className ) ? $a->className : '' ?>"><?php
+
+        if ( isset( $a->placeholder ) ) { ?>
+            <option value=""><?php echo $a->placeholder ?></option>
+        <?php } ?>
+
+        <?php foreach ( $a->options as $k => $v ) { ?>
+            <option
+                value="<?php echo esc_attr( $k ) ?>"
+                <?php echo isset( $a->value ) && $a->value == $k ? 'selected' : '' ?>
+            ><?php echo esc_attr( $v ) ?></option>
+        <?php } ?>
+    </select>
+    <?php
+}
+
+function input($a) {
+    ?>
+    <input type="text"
+        name="<?php echo FCPFSC['prefix'] . $a->name ?>"
+        id="<?php echo FCPFSC['prefix'] . $a->name ?>"
+        placeholder="<?php echo isset( $a->placeholder ) ? $a->placeholder  : '' ?>"
+        value="<?php echo isset( $a->value ) ? esc_attr( $a->value ) : '' ?>"
+        class="<?php echo isset( $a->className ) ? $a->className : '' ?>"
+    />
+    <?php
+}
+
+function get_meta_values( $key, $type = 'post' ) {
+
+    global $wpdb;
+
+    $metas = $wpdb->get_col( $wpdb->remove_placeholder_escape( $wpdb->prepare('
+
+        SELECT `post_id`
+        FROM `'.$wpdb->postmeta.'`
+        WHERE `meta_key` = %s AND `meta_value` LIKE %s
+
+    ', $key, $wpdb->add_placeholder_escape( '%"'.$type.'"%' ) ) ) );
+
+    return $metas;
+}
+
+//++ add blog as the archive
